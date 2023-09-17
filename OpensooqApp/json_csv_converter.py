d7a25f71fb6c4032f284e21a2d27bec61afb9075
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2023-09-17 19:03:01 +0500

    json to csv convertor

---
+++ OpensooqApp/json_csv_converter.py
@@ -0,0 +1,23 @@
+import json
+import csv
+import urllib.parse
+
+with open("OpensooqCleanData.json","r") as f:
+    data = json.loads(f.read())
+
+with open("Opensooq.csv", "w", newline="", encoding='utf-8') as csv_file:
+
+    csv_writer = csv.writer(csv_file)
+
+    csv_writer.writerow(data[0].keys())
+    counter = 1
+    for product in data:
+        
+        try:
+            print("Product#", counter)
+            csv_writer.writerow(product.values())
+            counter += 1
+        except Exception as e:
+            print("Error:",product["name"])
+            print(e)
+            
